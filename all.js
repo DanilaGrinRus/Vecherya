272f3d35e800b892ec484fcf281692ee5a68f94d
DanilaGrinRus <64834516+DanilaGrinRus@users.noreply.github.com>
2026-01-19 22:00:05 +0700

    Add files via upload

---
+++ all.js
@@ -0,0 +1,665 @@
+'use strict';
+/** Single-file logic for Cardpedia. Data embedded below as CARDS. */
+
+const CARDS = [
+  {
+    "id": "angel",
+    "name": "Angel",
+    "img": "cards/angel_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "antiekran",
+    "name": "Antiekran",
+    "img": "cards/antiekran_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "bank",
+    "name": "Bank",
+    "img": "cards/bank_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "bank_sivyi_merin",
+    "name": "Bank sivyi merin",
+    "img": "cards/bank_sivyi_merin_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "bankrot",
+    "name": "Bankrot",
+    "img": "cards/bankrot_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "bita",
+    "name": "Bita",
+    "img": "cards/bita_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "black",
+    "name": "Black",
+    "img": "cards/black_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "blue",
+    "name": "Blue",
+    "img": "cards/blue_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "capitalism_communism",
+    "name": "Capitalism communism",
+    "img": "cards/capitalism_communism_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "chameleon",
+    "name": "Chameleon",
+    "img": "cards/chameleon_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "chb",
+    "name": "Chb",
+    "img": "cards/chb_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "commission",
+    "name": "Commission",
+    "img": "cards/commission_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "content_card",
+    "name": "Content card",
+    "img": "cards/content_card_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "converter",
+    "name": "Converter",
+    "img": "cards/converter_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "emerald",
+    "name": "Emerald",
+    "img": "cards/emerald_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "exterminatus",
+    "name": "Exterminatus",
+    "img": "cards/exterminatus_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "gift_12min",
+    "name": "Gift 12min",
+    "img": "cards/gift_12min_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "gold",
+    "name": "Gold",
+    "img": "cards/gold_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "golden_scales",
+    "name": "Golden scales",
+    "img": "cards/golden_scales_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "grabiteli",
+    "name": "Grabiteli",
+    "img": "cards/grabiteli_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "gray",
+    "name": "Gray",
+    "img": "cards/gray_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "green",
+    "name": "Green",
+    "img": "cards/green_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "jackpot_cards",
+    "name": "Jackpot cards",
+    "img": "cards/jackpot_cards_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "joker",
+    "name": "Joker",
+    "img": "cards/joker_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "kost_1",
+    "name": "Kost 1",
+    "img": "cards/kost_1_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "kost_2",
+    "name": "Kost 2",
+    "img": "cards/kost_2_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "last_judgement_1",
+    "name": "Last judgement 1",
+    "img": "cards/last_judgement_1_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "last_judgement_2",
+    "name": "Last judgement 2",
+    "img": "cards/last_judgement_2_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "liftador",
+    "name": "Liftador",
+    "img": "cards/liftador_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "magnet",
+    "name": "Magnet",
+    "img": "cards/magnet_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "matreshki",
+    "name": "Matreshki",
+    "img": "cards/matreshki_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "mayak_bezishodnosti",
+    "name": "Mayak bezishodnosti",
+    "img": "cards/mayak_bezishodnosti_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "minus_1h",
+    "name": "Minus 1h",
+    "img": "cards/minus_1h_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "minus_3h",
+    "name": "Minus 3h",
+    "img": "cards/minus_3h_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "musical",
+    "name": "Musical",
+    "img": "cards/musical_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "obnulenie",
+    "name": "Obnulenie",
+    "img": "cards/obnulenie_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "obosran",
+    "name": "Obosran",
+    "img": "cards/obosran_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "pacific",
+    "name": "Pacific",
+    "img": "cards/pacific_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "pergament_blue",
+    "name": "Pergament blue",
+    "img": "cards/pergament_blue_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "pergament_gold",
+    "name": "Pergament gold",
+    "img": "cards/pergament_gold_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "pink",
+    "name": "Pink",
+    "img": "cards/pink_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "preispodnya",
+    "name": "Preispodnya",
+    "img": "cards/preispodnya_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "rainbow",
+    "name": "Rainbow",
+    "img": "cards/rainbow_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "red",
+    "name": "Red",
+    "img": "cards/red_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "ruby_bank",
+    "name": "Ruby bank",
+    "img": "cards/ruby_bank_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "ruby_kost_1",
+    "name": "Ruby kost 1",
+    "img": "cards/ruby_kost_1_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "ruby_kost_2",
+    "name": "Ruby kost 2",
+    "img": "cards/ruby_kost_2_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "ruby_plus3",
+    "name": "Ruby plus3",
+    "img": "cards/ruby_plus3_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "safe",
+    "name": "Safe",
+    "img": "cards/safe_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "sweets",
+    "name": "Sweets",
+    "img": "cards/sweets_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "temple",
+    "name": "Temple",
+    "img": "cards/temple_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "topor",
+    "name": "Topor",
+    "img": "cards/topor_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "tv_wheel",
+    "name": "Tv wheel",
+    "img": "cards/tv_wheel_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "tv_wheel_3",
+    "name": "Tv wheel 3",
+    "img": "cards/tv_wheel_3_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "tv_wheel_5",
+    "name": "Tv wheel 5",
+    "img": "cards/tv_wheel_5_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "vedro_egora",
+    "name": "Vedro egora",
+    "img": "cards/vedro_egora_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "wheel_eater_1",
+    "name": "Wheel eater 1",
+    "img": "cards/wheel_eater_1_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "wheel_eater_2",
+    "name": "Wheel eater 2",
+    "img": "cards/wheel_eater_2_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "wheel_eater_round",
+    "name": "Wheel eater round",
+    "img": "cards/wheel_eater_round_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "white",
+    "name": "White",
+    "img": "cards/white_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "yellow",
+    "name": "Yellow",
+    "img": "cards/yellow_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "zabanen",
+    "name": "Zabanen",
+    "img": "cards/zabanen_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  },
+  {
+    "id": "zamorozka",
+    "name": "Zamorozka",
+    "img": "cards/zamorozka_600.webp",
+    "category": "",
+    "rarity": "",
+    "emoji": "",
+    "rules": ""
+  }
+];
+
+const $ = (id) => document.getElementById(id);
+
+function setMeta(text){
+  const el = $('meta');
+  if (el) el.textContent = text;
+}
+
+function normalize(s){
+  return String(s || '').trim().toLowerCase();
+}
+
+function escapeHtml(str){
+  return String(str)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
+
+function openPanel(c){
+  const panel = $('panel');
+  $('pTitle').textContent = c.emoji ? `${c.emoji} ${c.name}` : c.name;
+  $('pImg').src = c.img || '';
+  $('pImg').alt = c.name || '';
+  $('pMeta').textContent = [c.id, c.category, c.rarity].filter(Boolean).join(' • ') || c.id;
+
+  if (c.rules && String(c.rules).trim()){
+    $('pText').innerHTML = `<div>${escapeHtml(c.rules).replace(/\n/g,'<br>')}</div>`;
+  } else {
+    $('pText').innerHTML = `<div class="muted">Описание и правила можно дополнять прямо в <code>all.js</code> в массиве <code>CARDS</code> (поле <code>rules</code>).</div>`;
+  }
+
+  panel.hidden = false;
+}
+
+function closePanel(){
+  $('panel').hidden = true;
+}
+
+function render(){
+  const q = normalize($('q').value);
+  const onlyWithImages = $('onlyWithImages').checked;
+
+  const list = CARDS.filter(c => {
+    const name = normalize(c.name);
+    const id = normalize(c.id);
+    const okQ = !q || name.includes(q) || id.includes(q);
+    const okImg = !onlyWithImages || !!c.img;
+    return okQ && okImg;
+  });
+
+  const container = $('cards');
+  container.innerHTML = '';
+
+  for (let i = 0; i < list.length; i++){
+    const c = list[i];
+    const el = document.createElement('article');
+    el.className = 'card';
+    el.setAttribute('data-id', c.id);
+
+    const title = c.emoji ? `${c.emoji} ${c.name}` : c.name;
+    const meta = [c.id, c.category, c.rarity].filter(Boolean).join(' • ');
+
+    el.innerHTML = `
+      <img class="card__img" src="${c.img}" alt="${c.name}" loading="lazy" decoding="async">
+      <div class="card__body">
+        <div class="card__title">${title}</div>
+        <div class="card__meta">${meta}</div>
+      </div>
+    `;
+
+    el.addEventListener('click', () => openPanel(c));
+    container.appendChild(el);
+  }
+
+  setMeta(`Показано: ${list.length} / ${CARDS.length}`);
+}
+
+function init(){
+  $('q').addEventListener('input', render);
+  $('onlyWithImages').addEventListener('change', render);
+  $('pClose').addEventListener('click', closePanel);
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') closePanel();
+  });
+
+  render();
+}
+
+init();
